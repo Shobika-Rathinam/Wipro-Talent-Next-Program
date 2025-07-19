600f80a00e7cdee53aadce150a26d3c721311db5
Shobika-Rathinam <shobikar2501@gmail.com>
2025-07-19 10:04:22 +0530

    Create sum_of_last.java

---
+++ sum_of_last.java
@@ -0,0 +1,12 @@
+class Main{
+  public int last(int n1,int n2)
+
+  {
+    return n1%10+n2%10;
+  }
+  public static void main(String args[])
+  {
+    
+      System.out.println(last(55,67));
+  }
+}
